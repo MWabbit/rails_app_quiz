579c25f879c0490e962cdc37c54a1a4f5aa53aa7
MWabbit <gonzalezjuly25@gmail.com>
2022-06-01 14:40:23 -0500

    Jobs controller added

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,9 @@
+class JobsController < ApplicationController
+    def index
+        render plain: "I don't know what I am doing but I think this is right."
+    end 
+
+    def create
+        render plain: "I still don't know what I am doing but I think this is right."
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/jobs', to: 'jobs#create'
 
-  get '/jobs/:id', to: 'static_pages#index'
+  get '/jobs/:id', to: 'jobs#index'
 
-  get '/jobs', to: 'static_pages#index'
+  get '/jobs', to: 'jobs#index'
 end
